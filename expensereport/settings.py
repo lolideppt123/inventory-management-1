530a723b4c5bdf287c5853c454763eed74a031cf
joseph piquero <j_piquero@fanshaweonline.ca>
2022-11-24 22:25:21 -0500

    uploaded cors headers

--- expensereport/settings.py
+++ expensereport/settings.py
@@ -182,3 +182,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+CSRF_TRUSTED_ORIGINS = ['https://metserv-inventory-management.up.railway.app/']
